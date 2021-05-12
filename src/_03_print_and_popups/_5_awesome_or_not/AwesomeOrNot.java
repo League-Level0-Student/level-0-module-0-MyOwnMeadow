01ea0a0639f647882a354d4d390c8488cb06497b
student <student@ip-172-31-51-101.us-west-2.compute.internal>
2021-05-11 18:27:39 -0700

    AWSOM!!!

--- src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
+++ src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
@@ -1,36 +1,32 @@
 package _03_print_and_popups._5_awesome_or_not;
 
 import java.util.Random;
+import javax.swing.JOptionPane;
 
 public class AwesomeOrNot {
-
-	// 1. Make a main method that includes everything below
-
 	
-		Random ran = new Random();    //This will be used below to make a random number. 
+	public static void main(String[] args) {
 		
-		// 2. Make a variable that will hold a random whole number
-	
-		// 3. Set your variable equal to a positive number less than 4 using     ran.nextInt(4); 
-	
-		// 3. Print your variable to the console
-	
-		// 4. Get the user to enter something that they think is awesome
-	
-		// 5. If your variable is  0
-	
-			// -- tell the user whatever they entered is awesome!
-	
-		// 6. If your variable is  1
-	
-			// -- tell the user whatever they entered is ok.
-	
-		// 7. If your variable is  2
-	
-			// -- tell the user whatever they entered is boring.
-	
-		// 8. If your variable is  3
-	
-			// -- invent your own message to give to the user (be nice).
-
+		Random ran = new Random();    //This will be used below to make a random number. 
+		int dom = ran.nextInt(4);
+		
+		System.out.println(dom);
+		
+		JOptionPane.showInputDialog("Tell us something that you think is awsom!");
+			if(dom == 0) {
+				JOptionPane.showMessageDialog(null, "That is apsuoluly AWSOM!!!");
+			}
+			else if(dom == 1) {
+				JOptionPane.showMessageDialog(null, "That is prety good.");
+			}
+			else if(dom == 2) {
+				JOptionPane.showMessageDialog(null, "...Huh? We all fell aslep... That was vary boring...");
+			}
+			else if(dom == 3) {
+				JOptionPane.showMessageDialog(null, "That is... BETER THEN EVERYTHING WE'VE EVER HEARD BEFORE!!!");
+			}
+			else {
+				JOptionPane.showMessageDialog(null,"...Huh? It seems as if the code is broken... This shouldn't even be happening!");
+			}
+	}
 }
